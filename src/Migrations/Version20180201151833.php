0b70b6b0d4663b809c00aeb85e1fdc8aef5bae21
Dan Abrey <danabrey@gmail.com>
2018-02-01 15:20:56 +0000

    Switch to mariadb, add migration to set up initial db, app.json for dokku to run migrations on push

---
+++ src/Migrations/Version20180201151833.php
@@ -0,0 +1,36 @@
+<?php declare(strict_types = 1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20180201151833 extends AbstractMigration
+{
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE team (id INT AUTO_INCREMENT NOT NULL, abbreviation VARCHAR(255) NOT NULL, name VARCHAR(255) NOT NULL, position INT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE prediction (id INT AUTO_INCREMENT NOT NULL, club_id INT DEFAULT NULL, entrant_id INT DEFAULT NULL, predicted_position INT NOT NULL, position_difference INT NOT NULL, points INT NOT NULL, INDEX IDX_36396FC861190A32 (club_id), INDEX IDX_36396FC88BFF9D26 (entrant_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE entrant (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, score INT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE prediction ADD CONSTRAINT FK_36396FC861190A32 FOREIGN KEY (club_id) REFERENCES team (id)');
+        $this->addSql('ALTER TABLE prediction ADD CONSTRAINT FK_36396FC88BFF9D26 FOREIGN KEY (entrant_id) REFERENCES entrant (id)');
+    }
+
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE prediction DROP FOREIGN KEY FK_36396FC861190A32');
+        $this->addSql('ALTER TABLE prediction DROP FOREIGN KEY FK_36396FC88BFF9D26');
+        $this->addSql('DROP TABLE team');
+        $this->addSql('DROP TABLE prediction');
+        $this->addSql('DROP TABLE entrant');
+    }
+}
