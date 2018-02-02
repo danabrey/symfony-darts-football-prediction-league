8d98501cf926a06096a8838bd31ae12c54790f29
Dan Abrey <danabrey@gmail.com>
2018-02-02 20:16:51 +0000

    turn on fixtures bundle in prod

--- config/bundles.php
+++ config/bundles.php
@@ -11,5 +11,5 @@ return [
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
-    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['all' => true],
 ];
